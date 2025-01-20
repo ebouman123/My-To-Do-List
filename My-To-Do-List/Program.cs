755f041027aba48830b759d4c1a13cc3163d1f60
Evan Bouman <evan.bouman@gmail.com>
2025-01-20 15:48:37 -0600

    Added GET to get by id

--- My-To-Do-List/Program.cs
+++ My-To-Do-List/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var todos = new List<Todo>();
 
+// returns either 200 with a Todo in the body, or 404
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) => 
+{
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null
+        ? TypedResults.NotFound()
+        : TypedResults.Ok(targetTodo);
+});
+
+// posts a task to the todos List
 app.MapPost("/todos", (Todo task) =>
 {
     todos.Add(task);
